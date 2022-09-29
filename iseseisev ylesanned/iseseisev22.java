1b37a887b5c132e7ef62b47ccfec0cd47a27f491
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-09-29 15:52:54 +0300

    Add files via upload

---
+++ iseseisev ylesanned/iseseisev22.java
@@ -0,0 +1,9 @@
+//Artjom Vinogradov
+
+public class iseseisev14 {
+
+
+	public static void main(String[] args) {
+        System.out.printf("Hello, %s!", "Michael Scott");
+        System.out.printf("Hello, %s!", "Jim");
+        System.out.printf("Hello, %s!", "Dwight");
