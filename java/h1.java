4001e58aa4ee95697cd2a9a9d83c5d5c99ae8bed
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-10-02 13:51:41 +0300

    Add files via upload

---
+++ java/h1.java
@@ -0,0 +1,26 @@
+public class h1 {
+    public static void main(String[] args) {
+		
+		/*nimi */
+		String nimi = new String();			
+		nimi = "Artjom Vinogradov";
+		/*kurs */
+		String kursus = new String();		
+		kursus = "IT-21";		
+		/*vanus */
+		int vanus = 16;					
+		/*pikkus */
+		float pikkus = 181;				
+		/*hüüdnimi */
+		String hüüdnimi = new String();		
+		hüüdnimi = "Nur sultan";
+		
+
+		
+		
+		
+		
+		System.out.println("Minu nimi on "+nimi+" Ma olen "+vanus+". aastat vana, opin Haapsalu Kutsehariduskeskuses "+kursus+" kursusel, olen "+pikkus+" cm, minu hiiiidnimi on "+hüüdnimi);			//prindin välja muutujad
+		//System.out.println(minuArv);		//üritasin väljastada muutujat kuid see saatis errori;
+}
+}
