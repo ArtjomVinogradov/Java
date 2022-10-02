b9f0acb358586dfd6240e81a55bded3a92a2aab9
ArtjomVinogradov <98739429+ArtjomVinogradov@users.noreply.github.com>
2022-10-02 12:09:55 +0300

    Add files via upload

--- iseseisev ylesanned/iseseisev18.java
+++ iseseisev ylesanned/iseseisev18.java
@@ -1,35 +1,14 @@
-//Artjom Vinogradov
-
 public class iseseisev18 {
-        public static void main(String[] args) {
 
-                int number=3;
-                int number1=5;
-                int number2=12;
-                int number3=4;  
-                System.out.print("Even number of "+number+": ");
-                System.out.print("Even number of "+number1+": ");
-                System.out.print("Even number of "+number2+": ");
-                System.out.print("Even number of "+number3+": ");
-                
-                for (int i=1; i<=number; i++) 
-                for (int i1=1; i1<=number1; i1++) 
-                for (int i2=1; i2<=number2; i2++) 
-                for (int i3=1; i3<=number3; i3++) 
-                {  
-                //logic to check if the number is even or not  
-                //if i%2 is equal to zero, the number is even  
-                if (i%2==0)   
-                {  
-                System.out.print(i + " ");  
-                }  
-                }  
-                }  
-                }  
-                
-                        
-                
-                 
-                        
-                
-                
+
+
+    public static void main(String args[]){  
+    int a[]={3,5,4,12,5};  
+    System.out.println("Not even numbers:");  
+    for(int i=0;i<a.length;i++){  
+    if(a[i]%2!=0){  
+    System.out.println(a[i]);  
+    }  
+    }  
+    }
+}  
